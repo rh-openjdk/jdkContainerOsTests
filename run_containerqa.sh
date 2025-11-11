936296e3d27ce62d311d8d7be75f5c59c0295e4e
Jiri Vanek <jvanek@redhat.com>
2025-11-11 14:44:00 +0100

    Running the suite

--- run_containerqa.sh
+++ run_containerqa.sh
@@ -53,5 +53,53 @@ do
   esac
 done
 
+if [ -z "$CONTAINER_VERSION" ] ; then
+  print_help >&2
+fi
+if  [ -n "$ARG_REPORT_DIR" ] ; then
+  if [ ! -e "$ARG_REPORT_DIR" ] ; then
+    echo "$ARG_REPORT_DIR do not exists"
+    exit 2
+  fi
+fi
+
 echo "Ensure that the run-folder-as-tests sub-repo is properly pulled down."
 echo "Ensure Podman is installed on the host under tests."
+echo "See test.yaml file if in doubts"
+
+
+TIME=$(date +%s)
+jtWork="test.${TIME}/jdk/work"
+jtReport="test.${TIME}/jdk/report"
+mkdir -p $jtWork
+mkdir -p $jtReport
+export SCRATCH_DISK="`pwd`/$jtWork"
+export WORKSPACE="`pwd`/$jtReport"
+SUITE_FOLDER="containersQa"
+bash  ${SCRIPT_DIR}/run-folder-as-tests/run-folder-as-tests.sh ${SCRIPT_DIR}/${SUITE_FOLDER} ${CONTAINER_VERSION}
+
+toPack="${jtReport} test.${TIME}/tests.log"
+if [ "x$JNI_PACK_WORK" == "xtrue" ] ; then
+toPack="$toPack ${jtWork}";
+fi
+tar -czf test.${TIME}.tar.gz  $toPack || echo "Packing of results tarball failed"
+if ! [ -f test.${TIME}/tests.log ] ; then
+	echo "Missing tests.log!" 1>&2
+	exit 1
+fi
+
+if  [ -n "$ARG_REPORT_DIR" ] ; then
+  if [ -e "$ARG_REPORT_DIR" ] ; then
+    mv -v test.${TIME}.* "$ARG_REPORT_DIR"
+  fi
+fi
+
+# results should be in log, if not, it means suite was not run
+grep -Eqi -e '^passed' -e '^(failed|error)' -e '^Ignored' test.${TIME}/tests.log || exit 2
+
+if [ "x$JNI_FAIL" == "xtrue" ] ; then
+  if grep -Eq -e '^Failed: [1-9]' test.${TIME}/tests.log  ; then
+    exit 1
+  fi
+fi
+
